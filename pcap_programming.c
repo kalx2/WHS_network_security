64cde98c189ea31c0eae57b6a9b6d4c1ff83b8bf
kalx2 <73418160+kalx2@users.noreply.github.com>
2023-09-23 21:36:35 +0900

    Create pcap_programming.c
    WHS network_security

---
+++ pcap_programming.c
@@ -0,0 +1,79 @@
+﻿#include <stdio.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/ether.h>
+
+void packet_handler(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
+    struct ether_header *eth_header = (struct ether_header *)packet;
+    struct ip *ip_header;
+    struct tcphdr *tcp_header;
+    char *payload;
+    int ip_header_len, tcp_header_len, payload_len;
+
+    if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
+        printf("Not an IP packet. Ignored.\n");
+        return;
+    }
+
+    ip_header = (struct ip *)(packet + ETHER_HDR_LEN);
+    ip_header_len = ip_header->ip_hl * 4;
+
+    if (ip_header->ip_p != IPPROTO_TCP) {
+        printf("Not a TCP packet. Ignored.\n");
+        return;
+    }
+
+    tcp_header = (struct tcphdr *)(packet + ETHER_HDR_LEN + ip_header_len);
+    tcp_header_len = tcp_header->doff * 4;
+    payload = (char *)(packet + ETHER_HDR_LEN + ip_header_len + tcp_header_len);
+    payload_len = pkthdr->len - (ETHER_HDR_LEN + ip_header_len + tcp_header_len);
+
+    printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth_header->ether_shost[0], eth_header->ether_shost[1],
+           eth_header->ether_shost[2], eth_header->ether_shost[3],
+           eth_header->ether_shost[4], eth_header->ether_shost[5]);
+
+    printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth_header->ether_dhost[0], eth_header->ether_dhost[1],
+           eth_header->ether_dhost[2], eth_header->ether_dhost[3],
+           eth_header->ether_dhost[4], eth_header->ether_dhost[5]);
+
+    printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
+    printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
+
+    printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
+    printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
+
+    printf("TCP Payload: %.*s\n", payload_len, payload);
+    printf("=======================================\n");
+}
+
+int main() {
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device: %s\n", errbuf);
+        return 1;
+    }
+
+    struct bpf_program fp;
+    char filter_exp[] = "tcp";
+    bpf_u_int32 net;
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return 1;
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return 1;
+    }
+
+    pcap_loop(handle, -1, packet_handler, NULL);
+
+    pcap_close(handle);
+    return 0;
+}
